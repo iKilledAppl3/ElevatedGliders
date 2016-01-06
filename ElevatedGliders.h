10b4bb32de96e84c19f24da9760daa117829a791
iKilledAppl3 <ipodnewbee234@icloud.com>
2016-01-06 09:45:57 -0500

    added comments

--- ElevatedGliders.h
+++ ElevatedGliders.h
@@ -1,22 +1,20 @@
+//Original by BigTimeLegend
+//Modified by iKilledAppl3
+
+//headers needed
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <libactivator/libactivator.h>
 
 
-static UIWindow *elevatedWindowMain;
-UIImageView *bird;
-UIImageView *backGround;
-int birdFlight;
-UIButton *myButton;
-NSTimer *birdFlying;
+static UIWindow *elevatedWindowMain; //Window for background view 
+UIImageView *bird; //bird image that shows on the screen
+UIImageView *backGround; //background image for Window
+int birdFlight; //birdflight integers for flight
+UIButton *myButton; //Exit button for user to tap to make the game view disappear
+NSTimer *birdFlying; //time that the bird takes flight in air
 
-
-
-
-
-
-
-
+//SpringBoard (Homescreen) View
 @interface SpringBoard 
 -(void)applicationDidFinishLaunching:(id)launching;
 @end 
